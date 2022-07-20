e0aadc42e39c733fa0d5351c1da8e339f489f47e
张旭东 <zhangxudong@xueguo.com>
2022-07-20 11:28:41 +0800

    feat(app.vue): 测试commitLint

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: CtrlC
+ * @Date: 2022-06-30 20:30:44
+ * @LastEditors: CtrlC
+ * @LastEditTime: 2022-07-20 11:24:33
+ * @Description: app入口文件
+ * @FilePath: /uni-preset-vue-vite/src/App.vue
+-->
+<script>
+export default {
+  onLaunch() {
+    console.log('App Launch')
+  },
+  onShow() {
+    console.log('App Show')
+  },
+  onHide() {
+    console.log('App Hide')
+  }
+}
+</script>
+
+<style lang="scss">
+/*每个页面公共css */
+@import '@/uni_modules/uview-plus/index.scss';
+</style>
