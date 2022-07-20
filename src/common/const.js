45301b32d3a9f0ea39ec1168b747329c29132881
SXZXD <635310636@qq.com>
2022-07-20 17:15:41 +0800

    feat(common): 添加const以及uviewconnfig

---
+++ src/common/const.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: CtrlC
+ * @Date: 2022-07-20 14:53:02
+ * @LastEditors: CtrlC
+ * @LastEditTime: 2022-07-20 14:53:02
+ * @Description: file content
+ * @FilePath: /uni-preset-vue-vite/src/common/const.js
+ */
---
+++ src/common/uviewConfing.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: CtrlC
+ * @Date: 2022-07-20 14:53:10
+ * @LastEditors: CtrlC
+ * @LastEditTime: 2022-07-20 14:53:10
+ * @Description: uview全局配置
+ * @FilePath: /uni-preset-vue-vite/src/common/uviewConfing.js
+ */
